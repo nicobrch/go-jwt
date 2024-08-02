fef5bd87cf137d24de0c6cd50d190a31e71d2bfc
Nico <nicolas.brch@gmail.com>
2024-08-02 13:29:57 -0400

    Revert "Include CORS and change route endpoints"
    This reverts commit fceee72664f6944fab2e94e4fbcc295bd9b20033.

--- server/main.go
+++ server/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 
-	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/nicobrch/go-jwt/controllers"
 	"github.com/nicobrch/go-jwt/initializers"
@@ -20,17 +19,15 @@ func init() {
 func main() {
 	app := gin.Default()
 
-	// React App routing
-	app.Use(cors.Default())
-
 	app.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{"message": "Hello, World!"})
 	})
 
-	// API routing
-	app.POST("/api/v1/signup", controllers.Signup)
-	app.POST("/api/v1/login", controllers.Login)
-	app.GET("/api/v1/validate", middleware.RequireAuth, controllers.Validate)
+	app.POST("/signup", controllers.Signup)
+
+	app.POST("/login", controllers.Login)
+
+	app.GET("/validate", middleware.RequireAuth, controllers.Validate)
 
 	app.Run()
 }
